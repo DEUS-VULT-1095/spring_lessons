8fe5ef3f726d2703d0071a63d18f84403751d21a
Denis Rogov <kpotuw@gmail.com>
2023-04-25 18:00:52 +0300

    lesson 12

--- src/main/java/ru/springcourse/ClassicalMusic.java
+++ src/main/java/ru/springcourse/ClassicalMusic.java
@@ -1,19 +1,20 @@
 package ru.springcourse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class ClassicalMusic implements Music {
-    private ClassicalMusic() {}
 
-    public static ClassicalMusic getClassicalMusic() {
-        return new ClassicalMusic();
-    }
-
+    @PostConstruct
     public void doMyInit() {
         System.out.println("Doing my initialization");
     }
 
+    @PreDestroy
     public void doMyDestroy() {
         System.out.println("Doing my destruction");
     }
